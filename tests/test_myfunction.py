1e93bb93609a836cdac702a7a1551bb8a1d69975
mukesh kumar <msingh.mukesh@gmail.com>
2022-01-29 15:51:33 +0100

    fdf

--- tests/test_myfunction.py
+++ tests/test_myfunction.py
@@ -1,3 +1,3 @@
 from MukeshCommonMLCodes.lib import try_me
 def test_try_me():
-    assert try_me() !="You have callmed Mukesh's functions"
+    assert try_me() =="You have callmed Mukesh's functions"
